76d4dafc6a881be514b6b692878962fb812d0d14
ilyes kooli <ilyes.kooli@elefant.ro>
2016-12-19 11:12:31 +0200

    Refactor ElefantPublicEventsExtension to extract common code

--- DependencyInjection/ElefantPublicEventsExtension.php
+++ DependencyInjection/ElefantPublicEventsExtension.php
@@ -31,52 +31,27 @@ class ElefantPublicEventsExtension extends Extension
             ->setDecoratedService('event_dispatcher')
             ->setArguments([new Reference(PublicEventDispatcher::ID . '.inner')]);
 
-
         foreach ($config['handlers'] as $key => $handler) {
             switch ($handler['type']) {
                 case 'logger':
-                    $this->loadLoggerHandler($key, $handler, $container);
+                    $this->loadLoggerHandler($key, $handler, $container, $handler['type']);
                     break;
                 case 'guzzle':
-                    $this->loadGuzzleHandler($key, $handler, $container);
+                    $this->loadGuzzleHandler($key, $handler, $container, $handler['type']);
                     break;
                 case 'rabbitmq_producer':
-                    $this->loadRabbitMqProducerHandler($key, $handler, $container);
+                    $this->loadRabbitMqProducerHandler($key, $handler, $container, $handler['type']);
                     break;
             }
         }
     }
 
-    private function loadLoggerHandler($name, array $config, ContainerBuilder $container)
+    private function loadLoggerHandler($name, array $config, ContainerBuilder $container, $type)
     {
-        $handlerDefinition = $container
-            ->register(sprintf('elefant.public_events.%s_handler', $name), LoggerHandler::class)
-            ->addMethodCall('setSerializer', [new Definition(PHPSerializer::class)])
-            ->addTag('elefant.public_events.handler', ['type' => 'logger']);
-
-        if (!isset($config['filters'])) {
-            $config['filters'] = [['name' => '/.*/']];
-        }
-
-        foreach ($config['filters'] as $index => $filter) {
-            if (!empty($filter['name'])) {
-                $filterDefinition = $container
-                    ->register(sprintf('elefant.public_events.%s_%s_name_filter', $name, $index), NameFilter::class)
-                    ->setArguments([$filter['name']]);
-                $handlerDefinition->addMethodCall('addFilter', [$filterDefinition]);
-            }
-            if (!empty($filter['class'])) {
-                $filterDefinition = $container
-                    ->register(sprintf('elefant.public_events.%s_%s_class_filter', $name, $index), ClassFilter::class)
-                    ->setArguments([$filter['class']]);
-                $handlerDefinition->addMethodCall('addFilter', [$filterDefinition]);
-            }
-        }
-
-        return $handlerDefinition;
+        return $this->loadHandler($name, $config, $container, LoggerHandler::class, $type);
     }
 
-    private function loadGuzzleHandler($name, array $config, ContainerBuilder $container)
+    private function loadGuzzleHandler($name, array $config, ContainerBuilder $container, $type)
     {
         $optionsResolver = new OptionsResolver();
 
@@ -88,34 +63,13 @@ class ElefantPublicEventsExtension extends Extension
 
         $guzzleConfig = $optionsResolver->resolve($config['config']);
 
-        $handlerDefinition = $container
-            ->register(sprintf('elefant.public_events.%s_handler', $name), GuzzleHandler::class)
-            ->setArguments([new Reference($guzzleConfig['client']), $guzzleConfig['method'], $guzzleConfig['uri'], $guzzleConfig['headers']])
-            ->addMethodCall('setSerializer', [new Definition(PHPSerializer::class)])
-            ->addTag('elefant.public_events.handler', ['type' => 'logger']);
-
-        if (!isset($config['filters'])) {
-            $config['filters'] = [['name' => '/.*/']];
-        }
-        foreach ($config['filters'] as $index => $filter) {
-            if (!empty($filter['name'])) {
-                $filterDefinition = $container
-                    ->register(sprintf('elefant.public_events.%s_%s_name_filter', $name, $index), NameFilter::class)
-                    ->setArguments([$filter['name']]);
-                $handlerDefinition->addMethodCall('addFilter', [$filterDefinition]);
-            }
-            if (!empty($filter['class'])) {
-                $filterDefinition = $container
-                    ->register(sprintf('elefant.public_events.%s_%s_class_filter', $name, $index), ClassFilter::class)
-                    ->setArguments([$filter['class']]);
-                $handlerDefinition->addMethodCall('addFilter', [$filterDefinition]);
-            }
-        }
+        $handlerDefinition = $this->loadHandler($name, $config, $container, GuzzleHandler::class, $type);
+        $handlerDefinition->setArguments([new Reference($guzzleConfig['client']), $guzzleConfig['method'], $guzzleConfig['uri'], $guzzleConfig['headers']]);
 
         return $handlerDefinition;
     }
 
-    private function loadRabbitMqProducerHandler($name, array $config, ContainerBuilder $container)
+    private function loadRabbitMqProducerHandler($name, array $config, ContainerBuilder $container, $type)
     {
         $optionsResolver = new OptionsResolver();
 
@@ -124,16 +78,23 @@ class ElefantPublicEventsExtension extends Extension
             ->setDefault('routing_key', $name);
 
         $producerConfig = $optionsResolver->resolve($config['config']);
+        $handlerDefinition = $this->loadHandler($name, $config, $container, GuzzleHandler::class, $type);
+        $handlerDefinition->setArguments([new Reference(sprintf('old_sound_rabbit_mq.%s_producer', $producerConfig['producer'])), $producerConfig['routing_key']]);
 
+        return $handlerDefinition;
+    }
+
+    private function loadHandler($name, array $config, ContainerBuilder $container, $handlerClass, $type)
+    {
         $handlerDefinition = $container
-            ->register(sprintf('elefant.public_events.%s_handler', $name), GuzzleHandler::class)
-            ->setArguments([new Reference(sprintf('old_sound_rabbit_mq.%s_producer', $producerConfig['producer'])), $producerConfig['routing_key']])
+            ->register(sprintf('elefant.public_events.%s_handler', $name), $handlerClass)
             ->addMethodCall('setSerializer', [new Definition(PHPSerializer::class)])
-            ->addTag('elefant.public_events.handler', ['type' => 'logger']);
+            ->addTag('elefant.public_events.handler', ['type' => $type]);
 
         if (!isset($config['filters'])) {
             $config['filters'] = [['name' => '/.*/']];
         }
+
         foreach ($config['filters'] as $index => $filter) {
             if (!empty($filter['name'])) {
                 $filterDefinition = $container
