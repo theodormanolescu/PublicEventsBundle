fc6fab25aaf3cf5eea18db09059bc4c54022c9ac
ilyes kooli <ilyes.kooli@elefant.ro>
2017-03-06 10:23:00 +0200

    fix failing test

--- Tests/PublicEvents/FormattersTest.php
+++ Tests/PublicEvents/FormattersTest.php
@@ -16,8 +16,8 @@ class FormatterTest extends TestCase
         $this->assertEquals(
             [
                 'event_name' => 'event_name',
-                'event' => (array)(new Event()),
                 'event_source' => ['trace'],
+                'event_class' => Event::class,
                 'hostname' => php_uname('n')
             ],
             $formatter->format(new PublicEvent('event_name', new Event(), ['trace']))
